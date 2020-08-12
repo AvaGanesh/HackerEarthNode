2840b354bb9fa5a14cfc4ec82883a0be53b1b8be
AvaGanesh <ava.ganesh@gmail.com>
2020-08-12 19:32:25 +0530

    Port address problem resolved

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require('path');
-const port = 8011 | process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname,'./Assignment')));
 app.get('*',function(req,res) {
